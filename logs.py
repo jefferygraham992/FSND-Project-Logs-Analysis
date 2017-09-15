f8880fdc56ee2cd312001237262b97e442ae695e
Jeffery Graham <jefferygraham992@gmail.com>
2017-09-15 15:27:01 -0400

    chore: refactor queries

--- logs.py
+++ logs.py
@@ -6,14 +6,10 @@
 import psycopg2
 from datetime import datetime
 
+
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=news")
-
-# What are the most popular three articles of all time?
-# Who are the most popular article authors of all time?
-# On which days did more than 1% of requests lead to errors?
-
 
 
 def countArticles():
@@ -34,9 +30,10 @@ def countAuthors():
     """Returns the most popular article authors of all time."""
     db = connect()
     cursor = db.cursor()
-    query = ("SELECT authors.name, count(authors.name) FROM log JOIN articles\n"
-    " ON log.path ILIKE '%' || articles.slug JOIN authors ON articles.author\n"
-    " = authors.id GROUP BY authors.name ORDER BY count DESC;")
+    query = ("SELECT authors.name, count(authors.name) FROM log JOIN\n"
+             " articles ON log.path ILIKE '%' || articles.slug JOIN authors\n"
+             " ON articles.author = authors.id GROUP BY authors.name ORDER BY\n"
+             " count DESC;")
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
@@ -48,15 +45,17 @@ def countErrors():
     """Returns the day(s) where more than 1% of requests led to errors."""
     db = connect()
     cursor = db.cursor()
-    query = ("SELECT errors.day, errors.count AS errors, sum_all_events.total\n"
-    " AS total, ((errors.count/sum_all_events.total) * 100) AS percent FROM\n"
-    " errors JOIN sum_all_events ON errors.day = sum_all_events.day WHERE\n"
-    " (errors.count/sum_all_events.total) * 100 > 1;")
+    query = ("SELECT errors.day, errors.count AS errors,\n"
+             " sum_all_events.total AS total,\n"
+             " ((errors.count/sum_all_events.total) * 100) AS percent FROM\n"
+             " errors JOIN sum_all_events ON errors.day = sum_all_events.day\n"
+             " WHERE (errors.count/sum_all_events.total) * 100 > 1;")
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
         date_object = datetime.strptime(result[0], '%m/%d/%Y')
-        print("%s - %.1f%% errors") % (date_object.strftime('%B %d, %Y'), float(result[3]))
+        print("%s - %.1f%% errors") % (date_object.strftime('%B %d, %Y'),
+            float(result[3]))
     db.close()
 
 
