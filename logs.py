5d7d9c87379fe9ed3d3c1f88f139f43f1e3c6cd5
Jeffery Graham <jefferygraham992@gmail.com>
2017-09-15 15:15:07 -0400

    chore: refactor queries

--- logs.py
+++ logs.py
@@ -20,12 +20,13 @@ def countArticles():
     """Returns the most popular three articles of all time."""
     db = connect()
     cursor = db.cursor()
-    cursor.execute("SELECT articles.title, count(articles.title) FROM log JOIN"
-    + " articles ON log.path ILIKE '%' || articles.slug GROUP BY articles.title "
-    + "ORDER BY count DESC LIMIT 3;")
+    query = ("SELECT articles.title, count(articles.title) FROM log JOIN\n"
+             " articles ON log.path ILIKE '%' || articles.slug GROUP BY\n"
+             " articles.title ORDER BY count DESC LIMIT 3;")
+    cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
-        print("%s - %s views") % (result[0], result[1])
+        print("\"%s\" - %s views") % (result[0], result[1])
     db.close()
 
 
@@ -33,9 +34,10 @@ def countAuthors():
     """Returns the most popular article authors of all time."""
     db = connect()
     cursor = db.cursor()
-    cursor.execute("SELECT authors.name, count(authors.name) FROM log JOIN "
-    + "articles ON log.path ILIKE '%' || articles.slug JOIN authors ON "
-    + "articles.author = authors.id GROUP BY authors.name ORDER BY count DESC;")
+    query = ("SELECT authors.name, count(authors.name) FROM log JOIN articles\n"
+    " ON log.path ILIKE '%' || articles.slug JOIN authors ON articles.author\n"
+    " = authors.id GROUP BY authors.name ORDER BY count DESC;")
+    cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
         print("%s - %s views") % (result[0], result[1])
@@ -46,7 +48,11 @@ def countErrors():
     """Returns the day(s) where more than 1% of requests led to errors."""
     db = connect()
     cursor = db.cursor()
-    cursor.execute("SELECT errors.day, errors.count AS errors, sum_all_events.total AS total, ((errors.count/sum_all_events.total) * 100) AS percent FROM errors JOIN sum_all_events ON errors.day = sum_all_events.day WHERE (errors.count/sum_all_events.total) * 100 > 1;")
+    query = ("SELECT errors.day, errors.count AS errors, sum_all_events.total\n"
+    " AS total, ((errors.count/sum_all_events.total) * 100) AS percent FROM\n"
+    " errors JOIN sum_all_events ON errors.day = sum_all_events.day WHERE\n"
+    " (errors.count/sum_all_events.total) * 100 > 1;")
+    cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
         date_object = datetime.strptime(result[0], '%m/%d/%Y')
